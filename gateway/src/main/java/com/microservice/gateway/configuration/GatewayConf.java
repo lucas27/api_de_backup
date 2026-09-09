63bf86a647a473f6c37cc7fe79acbb3a31517201
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-09 19:41:10 -0300

    feat: add routes

--- gateway/src/main/java/com/microservice/gateway/configuration/GatewayConf.java
+++ gateway/src/main/java/com/microservice/gateway/configuration/GatewayConf.java
@@ -1,5 +1,6 @@
 package com.microservice.gateway.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +8,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConf {
+    @Value("${gateway-api.host}")
+    private String host;
+     
+    
     @Bean 
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        System.out.print(host);
         return builder.routes()
-        .route("services", r -> r.path("/services/**")
-            .uri("http://localhost:8081"))
+        .route("auth", r -> r
+            .path("/auth/**")
+            .uri(host + ":8081"))
+        .route("services", r -> r
+            .path("/services/**")
+            // .filters(f -> f.tokenRelay())
+            .uri(host + ":8082"))
         .build();
     }
 }
