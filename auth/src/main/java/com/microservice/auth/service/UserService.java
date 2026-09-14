41e708c71045fc4a417fd8238b7e624dedccbc21
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-14 20:20:38 -0300

    feat: add user service

---
+++ auth/src/main/java/com/microservice/auth/service/UserService.java
@@ -0,0 +1,58 @@
+package com.microservice.auth.service;
+
+import java.util.Map;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.microservice.auth.dto.request.CreateUserDto;
+import com.microservice.auth.dto.request.LoginUserDto;
+import com.microservice.auth.dto.response.TokenDto;
+import com.microservice.auth.entity.User;
+import com.microservice.auth.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor 
+public class UserService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService service;
+    private final AuthenticationManager authenticationManager;
+   
+    public String createUser(CreateUserDto dto) {
+        String password = passwordEncoder.encode(dto.password());
+        User user = new User(dto, password);
+
+        repository.save(user);
+        return "criado com sucesso";
+    }
+    
+    public TokenDto login(LoginUserDto dto) {
+        var authentication = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+        Authentication auth = authenticationManager.authenticate(authentication);
+
+        Map<String, String> accessToken = service.generatedAcessToken(auth);
+        Map<String, String> refreshToken = service.generatedRefreshToken(auth); 
+
+        return new TokenDto(
+            accessToken.get("Access token"),
+            accessToken.get("expiry"),
+            refreshToken.get("Refresh token"),
+            refreshToken.get("expiry")
+        );
+    }
+
+    public Map<String, String> validationToken(String token) {
+        String formattedToken = token.replace("Bearer", "");
+        return service.generatedNewAccessToken(formattedToken);
+    }
+
+}
