a0ea9e8736fad39374415270a5186159a48af3c5
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-16 18:59:23 -0300

    feat: add entities

---
+++ files/src/main/java/com/microservice/files/entity/Data.java
@@ -0,0 +1,64 @@
+package com.microservice.files.entity;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.microservice.files.enums.Mimetype;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity 
+@Table (name = "Data")
+@AllArgsConstructor 
+@NoArgsConstructor 
+@Getter 
+@Setter 
+public class Data {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private Integer duration;
+
+    @Column(name = "file_length", unique = true, nullable = false)
+    private BigInteger fileLength;
+
+    @Enumerated(EnumType.STRING)
+    private Mimetype mimetype;
+
+    @Column(name = "physical_path", length = 100, unique = true, nullable = false)
+    private String physicalPath;
+    
+    @Column(name = "file_path", length = 100, unique = true, nullable = false)
+    private String filePath;
+    
+    @Column(name = "created_file", unique = true, nullable = false)
+    private LocalDateTime createdFile;
+    
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    @Column(name = "updated_at", updatable = true)
+    private LocalDateTime updatedAt;
+
+    @OneToOne(mappedBy = "file")
+    private File file;
+}
---
+++ files/src/main/java/com/microservice/files/entity/File.java
@@ -0,0 +1,64 @@
+package com.microservice.files.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.microservice.files.dto.FileDto;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+import lombok.Getter;
+// import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity 
+@Table (name = "File")
+// @AllArgsConstructor 
+// @NoArgsConstructor
+@Getter 
+@Setter  
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "data_id", referencedColumnName = "data_id",  nullable = false)
+    private Data data;
+    
+    @Column(length = 100, nullable = false)
+    private String name;
+    
+    @Column(length = 64, nullable = false, unique = true)
+    private String hash;
+
+    @CreationTimestamp
+    @Column(name = "created_at") 
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updateAt;
+    
+    public File(FileDto dto) {
+        this.setName(dto.name());
+        this.setHash(dto.hash());
+        this.data.setMimetype(dto.mimetype());
+        this.data.setDuration(dto.duration());
+        this.data.setFileLength(dto.fileLength());
+        this.data.setPhysicalPath(dto.physicalPath());
+        this.data.setFileLength(dto.fileLength());
+        this.data.setFilePath(dto.filePath());
+        this.data.setCreatedFile(dto.createdFile());
+    }
+}
