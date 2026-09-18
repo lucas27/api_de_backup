ed7dfcf16a67b57be21bc68831baa83502d9265d
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-18 18:53:38 -0300

    feat: create filter to hide route name

--- gateway/src/main/java/com/microservice/gateway/configuration/GatewayConf.java
+++ gateway/src/main/java/com/microservice/gateway/configuration/GatewayConf.java
@@ -6,21 +6,22 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class GatewayConf {
     @Value("${gateway-api.host}")
     private String host;
-     
-    
+
     @Bean 
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        System.out.print(host);
         return builder.routes()
         .route("auth", r -> r
-            .path("/auth/**")
+            .path("/api/v1/auth/**")
+            .filters(f -> f.rewritePath("/api/v1/auth/(?<segment>.*)", "/auth/${segment}"))
             .uri(host + ":8081"))
         .route("services", r -> r
-            .path("/services/**")
+            .path("/api/v1/services/**")
+            .filters(f -> f.rewritePath("/api/v1/services/(?<segment>.*)", "/services/${segment}"))
             // .filters(f -> f.tokenRelay())
             .uri(host + ":8082"))
         .build();
