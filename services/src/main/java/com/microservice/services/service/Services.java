ac5c9be986a8b8a22af064db40b2a57e63f8f79b
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-23 18:53:57 -0300

    feat: add feign client route and create data file

--- services/src/main/java/com/microservice/services/service/Services.java
+++ services/src/main/java/com/microservice/services/service/Services.java
@@ -1,8 +1,28 @@
 package com.microservice.services.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.microservice.services.client.ServicesClient;
+import com.microservice.services.dto.FileDto;
+import com.microservice.services.dto.UploadDto;
 
 @Service 
 public class Services {
-    
+    private ServicesClient client;
+
+    public Services(ServicesClient client) {
+        this.client = client;
+    }
+
+    public String uploadDataFile(
+        MultipartFile file,
+        Integer chunk,
+        Integer totalChunk,
+        UploadDto uploadDto
+    ) {
+        FileDto data = FileDto.createDataFile(uploadDto);
+        String resp = client.storageFile(file, chunk, totalChunk, data);
+        return resp;
+    }
 }
