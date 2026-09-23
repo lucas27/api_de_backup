d93a5e7e43a4f2e30b4a37623f9b9b87da98119d
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-23 18:52:49 -0300

    feat: add service

--- services/src/main/java/com/microservice/services/controller/ServicesController.java
+++ services/src/main/java/com/microservice/services/controller/ServicesController.java
@@ -1,12 +1,5 @@
 package com.microservice.services.controller;
 
-import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,21 +10,19 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.microservice.services.client.ServicesClient;
 import com.microservice.services.dto.UploadDto;
+import com.microservice.services.service.Services;
 
+import lombok.RequiredArgsConstructor;
 
 
 @Controller
 @RestController
-@RequestMapping("/services") 
+@RequestMapping("/services")
+@RequiredArgsConstructor
 public class ServicesController {
-    private ServicesClient client;
-
-    public ServicesController(ServicesClient client) {
-        this.client = client;
-    }
-
+    private final Services service;
+    
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> upload(
         @RequestPart("file") MultipartFile file,
@@ -39,7 +30,7 @@ public class ServicesController {
         @RequestParam("totalChunks") Integer total,
         @RequestPart("data") UploadDto uploadDto
     ) {
-        String resp = client.storageFile(file, chunk, total, uploadDto);
+        String resp = service.uploadDataFile(file, chunk, total, uploadDto);
         return ResponseEntity.ok().body(resp);
     }
 }
