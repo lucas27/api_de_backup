f71c62561ac5ddd3677d3187b410f89886d54cab
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-24 20:28:42 -0300

    remove: hash

--- services/src/main/java/com/microservice/services/dto/FileDto.java
+++ services/src/main/java/com/microservice/services/dto/FileDto.java
@@ -2,13 +2,11 @@ package com.microservice.services.dto;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 import com.microservice.services.enums.Mimetype;
 
 public record FileDto(
     String name,
-    String hash,
     Mimetype mimetype,
     Integer duration,
     BigInteger fileLength,
@@ -18,7 +16,6 @@ public record FileDto(
     public static FileDto createDataFile(UploadDto dto) {
         return new FileDto(
             dto.name(), 
-            UUID.randomUUID().toString(), 
             dto.mimetype(), 
             dto.duration(),  
             dto.fileLength(), 
