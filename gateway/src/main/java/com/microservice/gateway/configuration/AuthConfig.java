65972adac2a0149895084b745a064bd630ab814b
lucas27 <lucasbrasileiro27@gmail.com>
2026-09-25 20:11:16 -0300

    feat: add jwt decoder

--- gateway/src/main/java/com/microservice/gateway/configuration/AuthConfig.java
+++ gateway/src/main/java/com/microservice/gateway/configuration/AuthConfig.java
@@ -1,15 +1,23 @@
 package com.microservice.gateway.configuration;
 
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity   
 public class AuthConfig {
+    @Value("${api.public-key}")
+    private RSAPublicKey publicKey;
+
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
         return http
@@ -22,4 +30,8 @@ public class AuthConfig {
         .build();
     }
     
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
 }
